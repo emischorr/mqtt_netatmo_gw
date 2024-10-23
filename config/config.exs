de34cdecb1594850c403fe0b91fd5f1580f002d9
Enrico Mischorr <enrico@mischorr.de>
2024-10-23 21:26:37 +0200

    increase update interval to 5 min

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,6 @@ config :logger, :console,
 config :tesla, adapter: {Tesla.Adapter.Mint, timeout: 5_000}
 
 config :mqtt_netatmo_gw, :weather_station,
-  update_interval: 60_000
+  update_interval: 5*60*1000
 
 import_config "#{config_env()}.exs"
